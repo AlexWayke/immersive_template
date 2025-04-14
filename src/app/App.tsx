2c84382cd9b133db27ad73a90d72f06cb56d84b0
Alexander <105300503+AlexWayke@users.noreply.github.com>
2025-04-14 21:12:12 +0400

    Init: initial project

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae labore reprehenderit adipisci consequatur deleniti iure. Dolorum magni tempora dolorem ex consectetur, ipsam amet iusto eius cupiditate aliquid sit asperiores expedita.
+        </div>
+    </>
+  )
+}
+
+export default App
